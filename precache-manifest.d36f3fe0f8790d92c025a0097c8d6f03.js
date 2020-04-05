dc71a1cb910418f1f6289c9788abb04de86fcfb0
Aldi Zhupani <azhupani390@berkeley.edu>
2020-04-05 17:45:29 -0500

    Updates

---
+++ precache-manifest.d36f3fe0f8790d92c025a0097c8d6f03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23077d607397b4370e3a5c02fb70027",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "28891a3d6a49b8763672",
+    "url": "/dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4be41dacd03018039e5",
+    "url": "/dashboard/static/js/2.67b56251.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dashboard/static/js/2.67b56251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28891a3d6a49b8763672",
+    "url": "/dashboard/static/js/main.9e23da7e.chunk.js"
+  },
+  {
+    "revision": "9a34a5c47afc48018561",
+    "url": "/dashboard/static/js/runtime-main.5fa48c14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
