82b2cc879fe76ecb003c7ac0fc4541464e03485b
Aldi Zhupani <azhupani390@berkeley.edu>
2021-01-10 14:06:12 -0600

    Basic stock ticker tracer using canvasjs

--- src/Fetcher/Fetcher.js
+++ src/Fetcher/Fetcher.js
@@ -1,7 +1,13 @@
+// Documentation for canvas js: https://canvasjs.com/docs
+// Documentationf for alpha advantage: https://www.alphavantage.co/documentation/
+
 import React, { Fragment, useState, useEffect, useReducer } from "react";
 import axios from "axios";
+import Canvas from "./Canvas"
 
-const initialData = { items: [] }
+const initialData = { data: { "Meta Data": { 'symbol': 'C'}, "Time Series (Daily)": {} }}
+const APLHA_API_KEY = '1PXX8A1J2QJQFTBP'
+const STOCK_SYMBOL = 'C'
 
 const dataFetchReducer = (state, action) => {
   switch (action.type) {
@@ -43,8 +49,7 @@ const useDataApi = (initialUrl) => {
       dispatch({ type: "FETCH_INIT" });
       try {
         const result = await axios(url);
-        let items = result.data.items || [{}]
-        dispatch({ type: "FETCH_SUCCESS", payload: {...result.data, items} });
+        dispatch({ type: "FETCH_SUCCESS", payload: result });
       } catch (error) {
         dispatch({ type: "FETCH_FAILURE" });
       }
@@ -61,23 +66,28 @@ const useDataApi = (initialUrl) => {
 };
 
 function App() {
-  const [query, setQuery] = useState("isbn:0747532699");
-  const { data, isLoading, isError, doFetch } = useDataApi(
-    `https://www.googleapis.com/books/v1/volumes?q=${query}`
+  const [symbol, setSymbol] = useState(STOCK_SYMBOL);
+
+  const { data: { data }, isLoading, isError, doFetch } = useDataApi(
+    `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&interval=Daily&apikey=${APLHA_API_KEY}`
   );
+
+  useEffect(() => {
+    doFetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&interval=Daily&apikey=${APLHA_API_KEY}`);
+  }, [symbol, doFetch])
 
   return (
     <Fragment>
       <form
         onSubmit={event => {
-          doFetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+          doFetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&interval=Daily&apikey=${APLHA_API_KEY}`);
           event.preventDefault();
         }}
       >
         <input
           type="text"
-          value={query}
-          onChange={event => setQuery(event.target.value)}
+          value={symbol}
+          onChange={event => setSymbol(event.target.value)}
         />
         <button type="submit">Search</button>
       </form>
@@ -87,15 +97,11 @@ function App() {
       {isLoading ? (
         <div>Loading ...</div>
       ) : (
-        <ul>
-          {data.items.map(item => (
-            (item.id) ?
-              <li key={item.id}>
-                <a href={item.selfLink} target="_blank" rel="noopener noreferrer">{item.volumeInfo.title}</a>
-              </li>
-              : <p>No results found</p>
-          ))}
-        </ul>
+        <div>
+          <p>{data["Meta Data"] && data["Meta Data"].symbol}</p>
+          {/* <p>{data["Time Series (5min)"]}</p> */}
+          <Canvas symbol={symbol} data={data} />
+        </div>
       )}
     </Fragment>
   );
