b220ee1a486267af533645575e923d6f9858b9ae
aldizh <aldizhupani@gmail.com>
2021-02-24 20:01:52 -0600

    Refactor react router logic to redirect to correct route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
-import { Grid, Paper, Typography } from '@material-ui/core';
+import { Link, Route } from 'react-router-dom'
+import { Grid, Paper } from '@material-ui/core';
 import StcokFetcher from 'StockFetcher'
 import Table from 'Components/ExpandableTable'
 import ListPage from 'ListPage'
@@ -27,33 +27,32 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Route exact={true} path="/dashboard" render={() => (
-          <React.Fragment>
-            <NavBar />
-            <hr />
-            <p className="titleDesc">React Projects</p>
-            <Grid container justify="center" className={classes.root} spacing={3}>
-              <Grid container justify="center" item xs={12} sm={4} lg={4}>
-                <Paper className={classes.paper} elevation={0}>
-                  <Link to={'/dashboard/stocks'}>Stock Tracker</Link>
-                </Paper>
-              </Grid>
-              <Grid container justify="center" item xs={12} sm={4} lg={4}>
-                <Paper className={classes.paper}>
-                  <Link to={'/dashboard/expand_table'}>Expandable Table</Link>
-                </Paper>
-              </Grid>
-              <Grid container justify="center" item xs={12} sm={4} lg={4}>
-                <Paper className={classes.paper}>
-                <Link to={'/dashboard/filter_table'}>List Page</Link>
-                </Paper>
-              </Grid>
+      <Route exact={true} path="/" render={() => (
+        <React.Fragment>
+          <NavBar />
+          <hr />
+          <p className="titleDesc">React Projects</p>
+          <Grid container justify="center" className={classes.root} spacing={3}>
+            <Grid container justify="center" item xs={12} sm={4} lg={4}>
+              <Paper className={classes.paper} elevation={0}>
+                <Link to={'/stocks'}>Stock Tracker</Link>
+              </Paper>
             </Grid>
-          </React.Fragment>
+            <Grid container justify="center" item xs={12} sm={4} lg={4}>
+              <Paper className={classes.paper}>
+                <Link to={'/expand_table'}>Expandable Table</Link>
+              </Paper>
+            </Grid>
+            <Grid container justify="center" item xs={12} sm={4} lg={4}>
+              <Paper className={classes.paper}>
+              <Link to={'/filter_table'}>List Page</Link>
+              </Paper>
+            </Grid>
+          </Grid>
+        </React.Fragment>
         )}
       />
-      <Route path="/dashboard/stocks"
+      <Route exact path="/stocks"
         render={() => (
           <React.Fragment>
             <NavBar />
@@ -62,7 +61,7 @@ function App() {
           </React.Fragment>
         )}
       />
-      <Route path="/dashboard/expand_table" render={() => (
+      <Route exact path="/expand_table" render={() => (
           <React.Fragment>
             <NavBar />
             <hr />
@@ -70,7 +69,7 @@ function App() {
           </React.Fragment>
         )}
       />
-      <Route path="/dashboard/filter_table" render={() => (
+      <Route exact path="/filter_table" render={() => (
           <React.Fragment>
             <NavBar />
             <hr />
@@ -78,7 +77,6 @@ function App() {
           </React.Fragment>
         )}
       />
-      </Router>
     </div>
   );
 }
